ef5500f271f0b3f821ba33042bb6d052b2e25fba
Kholod13 <vlad13holod@gmail.com>
2025-05-08 23:18:22 +0200

    add modal window and create logic

--- components/Main.js
+++ components/Main.js
@@ -1,6 +1,8 @@
-import {Text, View, StyleSheet, ScrollView, Button, FlatList, TouchableOpacity, Image } from 'react-native';
+import {Text, View, StyleSheet, ScrollView, Button, FlatList, TouchableOpacity, Image, Modal } from 'react-native';
 import { gStyle } from '../styles/style';
 import React, { useState } from 'react';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function Main({navigation}) {
     const [news, setNews] = useState([
@@ -8,9 +10,20 @@ export default function Main({navigation}) {
       {name: 'Apple', anons: 'Apple is a tech giant renowned for its sleek design, user-friendly products, and cult-like brand loyalty.', full: 'Apple is a tech giant renowned for its sleek design, user-friendly products, and cult-like brand loyalty. From the groundbreaking iPhone to the powerful MacBook lineup, Apple has redefined how people interact with technology. With its closed ecosystem, seamless software-hardware integration, and iconic minimalist aesthetic, Apple continues to shape trends in consumer electronics, software, and services. Whether youre editing videos on a Mac or checking your health stats on an Apple Watch, Apple is always just a tap away.', key: '2', img: 'https://1291668043.rsc.cdn77.org/getthumbnail.aspx?q=60&crop=1&h=450&w=800&id_file=690941832'},
       {name: 'Facebook', anons: 'Facebook, now part of Meta Platforms Inc., began as a simple social networking site for college students and evolved into one of the most influential digital platforms in history.', full: 'Facebook, now part of Meta Platforms Inc., began as a simple social networking site for college students and evolved into one of the most influential digital platforms in history. With billions of users worldwide, Facebook connects people across continents, fuels community discussions, and shapes how news and trends spread globally. Despite controversies over data privacy and content moderation, its impact on communication, business, and culture remains immense. Whether sharing a memory, joining a group, or scrolling through the feed, Facebook continues to be a digital reflection of modern social life.', key: '3', img: 'https://www.newstream.cz/uploads/article/660x464_fit/iStock-500552643.jpg'},
     ]);
+
+    const [modalVisible, setModalWindow] = useState(false);
     return (
       <ScrollView showsVerticalScrollIndicator={false}>
+        <Modal visible={modalVisible} animationType='slide'>
+          <View>
+            <Text style={gStyle.title}>Add News</Text>
+          </View>
+          <MaterialCommunityIcons style={styles.add} name="close-box" size={44} color="black" 
+          onPress={()=> setModalWindow(false)} />
+        </Modal>
         <View style={gStyle.main}>
+          <MaterialIcons name="add-box" color="black" style={styles.add} size={44} 
+          onPress={()=> setModalWindow(true)}/>
           <Text style={gStyle.title}>News</Text>
           <FlatList data={news} renderItem={({item}) => (
             <TouchableOpacity style={styles.container} onPress={() => navigation.navigate('FullInfo', {news: item})}>
@@ -25,6 +38,9 @@ export default function Main({navigation}) {
 }
 
 const styles = StyleSheet.create({
+    add:{
+      textAlign: 'center',
+    },
       text: {
         fontSize: 26,
         fontFamily: 'mt-light',
