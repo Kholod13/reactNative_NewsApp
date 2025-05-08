1ee44750db7a871623ea6bb8dc427d89414111b5
Kholod13 <vlad13holod@gmail.com>
2025-05-08 23:29:51 +0200

    import library "Formik"

---
+++ components/Form.js
@@ -0,0 +1,17 @@
+import {Text, View, StyleSheet, ScrollView } from 'react-native';
+import { gStyle } from '../styles/style';
+import React, { useState } from 'react';
+import {Formik} from 'formik';
+
+export default function FullInfo({route}) {
+
+    return (
+      <View>
+        
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+        
+    });
